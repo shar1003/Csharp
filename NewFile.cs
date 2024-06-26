e9a9af4e114d0404af2c64abe8b9fe4990986546
Sharath Chandra Siluveru <sharath@frontwell-solutions.com>
2024-06-26 14:45:55 +0200

    four

--- NewFile.cs
+++ NewFile.cs
@@ -5,6 +5,10 @@ namespace Sampleproject
     public class MainClass
     {
         public static void Main(string[] args) 
-        { 
+        {
+            var num = 1;
+            var two = 2;
+            var threee = 3;
+            var four = 4;
         }
 }
