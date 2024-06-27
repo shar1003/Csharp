e01ce01e6dd82ae00f1ac0d264a8791c2fc2d103
Sharath Chandra Siluveru <sharath@frontwell-solutions.com>
2024-06-27 14:26:56 +0200

    debugg

--- NewFile.cs
+++ NewFile.cs
@@ -1,4 +1,8 @@
-using System:
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
 
 namespace Sampleproject
 {
@@ -7,11 +11,47 @@ namespace Sampleproject
         int i;
         int j;
 
-        public static void Main(string[] args) 
+        public int num;
+
+        public class clas
+        {
+
+        }
+
+        public static void Main(string[] args)
         {
             var num = 1;
             var two = 2;
-            var threee = 3;
+            var three = 3;
             var four = 4;
+
+            var sum = four + two;
+
+            MainClass ex = new MainClass();
+            int number = ex.num;
+            var cls =  new clas();
+
+            Console.WriteLine("sum of two numbers is {0}", sum);
+
+            if (sum == 3)
+            {
+                Console.WriteLine("happy path");
+            }
+            else
+            {
+                throw new Exception("unhappy path");
+            }
+
+            //try
+            //{
+            //    sum = num + two;
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Happy Path");
+            //}
+
+
         }
+    }
 }
