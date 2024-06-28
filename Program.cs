631a1d825f872742b16de27c9295b031fed7964c
Sharath Chandra Siluveru <sharath@frontwell-solutions.com>
2024-06-28 16:45:18 +0200

    staged

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+
+namespace Csharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Type t = typeof(Program);
+        }
+    }
+}
