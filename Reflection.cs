6c4fd8db60a5445d396060de09874e125bf3b915
Sharath Chandra Siluveru <sharath@frontwell-solutions.com>
2024-07-02 15:03:12 +0200

    changes

---
+++ Reflection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Diagnostics;
+
+
+namespace Sampleproject
+{
+    public class Program
+    {
+        public static void Reflection(string[] args)
+        {
+            Type t = typeof(Program);
+
+            MemberInfo[] members = t.GetMembers();
+            foreach (MemberInfo member in members)
+            {
+                Debug.WriteLine("Member = " + members);
+            }
+        }
+    }
+}
